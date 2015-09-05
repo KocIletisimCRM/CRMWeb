dbf6cfedab61bffd5e9cc45a3c74334ed9c9d4f7
kociletisim <yazilim@kociletisim.com.tr>
2015-09-05 18:40:06 +0300

    .

---
+++ Scripts/denemesil.js
@@ -0,0 +1,25 @@
+﻿var filter = {
+    filter: {
+        tableName: "taskqueue",
+        keyField: "taskorderno",
+        subTables: {
+            "attachedobjectid": {
+                tableName: "customer",
+                keyField: "customerid",
+                subTables: {
+                    "blockid": {
+                        tableName: "block",
+                        keyField: "blockid",
+                        subTables: {
+                            "siteid": {
+                                tableName: "site",
+                                keyField: "siteid",
+                                fieldFilters: [{fieldName: "region", op: 6, value: "beş"}]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
